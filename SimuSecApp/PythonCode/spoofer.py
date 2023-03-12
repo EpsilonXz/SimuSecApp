29648fc3a8af575c1b8d9518140874fca9d68bac
EpsilonXz <roicarm7@gmail.com>
2023-03-12 08:47:46 +0200

    Changed vulns

--- SimuSecApp/PythonCode/spoofer.py
+++ SimuSecApp/PythonCode/spoofer.py
@@ -7,7 +7,7 @@ class Spoofer:
         self.server = "smtp-relay.sendinblue.com"
         self.port = 587
 
-        self.spoof("andrewtate@gmail.com", "shalevshagan1@gmail.com", "Breath Air", "Vape is for losers! join: jointherealworld.com", "Andrew Tate")
+        self.spoof("andrewtate@tate.com", "shalevshagan1@gmail.com", "Breath Air", "Vape is for losers! join: jointherealworld.com", "Andrew Tate")
 
     def spoof(self, from_addr, to_addr, Header, Body, sender_full_fake_name):
         os.system(f'sendemail -xu {self.ZW1haWw} -xp {self.TWFzdGVyIFBhc3N3b3Jk} -s {self.server}:{self.port}\
